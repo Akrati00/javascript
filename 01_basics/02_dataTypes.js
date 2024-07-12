c61effb8d6241a525f53efabdfe0a863f763bb1a
Akrati Bhadauria <122599551+Akrati00@users.noreply.github.com>
2024-07-12 10:13:00 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+console.log(3
+    +3
+) // code readability should be high
+
+console.log("Akrati")
+
+let name = "Akrati" // string
+let age = 18        // number
+let isLoggedIn = false   // boolean
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof "akrati");
+console.log(typeof age);
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
+
+
+
+
