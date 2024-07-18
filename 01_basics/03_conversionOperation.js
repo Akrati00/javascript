145ac45bbc33d6529e00c9dc49966464b082dba3
Akrati Bhadauria <122599551+Akrati00@users.noreply.github.com>
2024-07-18 11:22:40 +0000

    .

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,4 +16,14 @@ console.log(valueInNumber); // NaN
 
 let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
