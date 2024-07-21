23ef4f33899de03d2fd1b2e6a9a95185078e7e72
Akrati Bhadauria <122599551+Akrati00@users.noreply.github.com>
2024-07-21 13:42:45 +0000

    .

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -13,4 +13,10 @@ console.log(null >= 0);  // true
 
 /* the reason is that an equality check == and comparisons > < >= <= work differently. Comaprisons convert null to a number , treating it as 0. that's why (3) null >= 0 is true and (1) null > 0 is false. */
 
-console.log()
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+console.log("2" === 2); // false
+console.log("2" == 2);  // true
