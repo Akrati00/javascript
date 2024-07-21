16476026a2078b7f8c5b29345b24ab76641a3334
Akrati Bhadauria <122599551+Akrati00@users.noreply.github.com>
2024-07-21 16:38:09 +0000

    .

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,26 @@
+// Primitive 
+
+// 7 types: String, Number, Boolean, Null, Undefined, Symbol, BigInt 
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+
+// NonPrimitive / reference type
+
+// Arrays, Objects, Functions 
+
+// const score:number = 100 // typescript
+
+
+
+
+
+
+
+
+
